f912dc121af9b6da285943e780a72a4993f0da61
Rahul <rahulraveendran@accubits.com>
2019-11-07 22:35:31 +0530

    options added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,28 @@ export class AppComponent {
     sliderUncheckedColor : "#bdbdbd",
     barDisabledBgColor: "red"
   }
+
+  settings2 = {
+    barCheckedBgColor : "#f45905",
+    sliderCheckedColor : "#c70d3a",
+    barUncheckedBgColor : "#ded5d5",
+    sliderUncheckedColor : "#bdbdbd",
+    barDisabledBgColor: "red"
+  }
+
+  settings3 = {
+    barCheckedBgColor : "#bc4873",
+    sliderCheckedColor : "#472b62",
+    barUncheckedBgColor : "#ded5d5",
+    sliderUncheckedColor : "#bdbdbd",
+    barDisabledBgColor: "red"
+  }
+
+  settings4 = {
+    barCheckedBgColor : "#00918e",
+    sliderCheckedColor : "#ffdc34",
+    barUncheckedBgColor : "#ded5d5",
+    sliderUncheckedColor : "#bdbdbd",
+    barDisabledBgColor: "red"
+  }
 }
